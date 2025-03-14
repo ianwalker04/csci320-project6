4f830f41ce375cd48ac2ee115a765199abf11ca7
ianwalker04 <ian04walker@gmail.com>
2025-03-13 21:41:26 -0500

    Finished everything except sound effects.

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ pub enum DebrisStatus {
 #[derive(Clone, Eq, PartialEq)]
 pub struct SpaceDebrisGame {
     player: Player,
-    debris: Vec<Debris, 10>,
+    debris: Vec<Debris, 30>,
     score: u32,
     spawn_countdown: u32,
     seed_count: u32
@@ -53,7 +53,8 @@ pub struct Player {
 pub struct Debris {
     col: usize,
     row: usize,
-    dx: isize,
+    dx: usize,
+    dx_tick: usize,
     color: Color,
     debris_status: DebrisStatus
 }
@@ -76,7 +77,7 @@ impl Default for SpaceDebrisGame {
             player: Player::default(),
             debris: Vec::new(),
             score: 0,
-            spawn_countdown: 50,
+            spawn_countdown: 5,
             seed_count: 0
         }
     }
@@ -98,8 +99,9 @@ impl Debris {
         let mut rng: Rand32 = Rand32::new(num.into());
         Self {
             col: BUFFER_WIDTH - 1,
-            row: rng.rand_range(0..(BUFFER_HEIGHT - 5) as u32) as usize,
-            dx: 1,
+            row: rng.rand_range(2..BUFFER_HEIGHT as u32) as usize,
+            dx: rng.rand_range(1..5) as usize,
+            dx_tick: 0,
             color: DEBRIS_COLORS[rng.rand_range(0..13) as usize],
             debris_status: DebrisStatus::Normal
         }
@@ -118,7 +120,7 @@ impl SpaceDebrisGame {
                 }
             }
         }
-        let mut deleted_debris: Vec<usize, 10> = Vec::<usize, 10>::new();
+        let mut deleted_debris: Vec<usize, 30> = Vec::<usize, 30>::new();
         for i in 0..self.debris.len() {
             if let Some(event) = self.debris[i].tick(&mut self.player) {
                 match event {
@@ -144,7 +146,7 @@ impl SpaceDebrisGame {
         self.seed_count += 1;
         if self.spawn_countdown == 0 {
             let _ = self.debris.push(Debris::new(self.seed_count));
-            self.spawn_countdown = 10;
+            self.spawn_countdown = 5;
         } else {
             self.spawn_countdown -= 1;
         }
@@ -190,7 +192,7 @@ impl SpaceDebrisGame {
         self.player.clear_current();
         clear_row(0, Color::Black);
         clear_row(1, Color::Black);
-        let mut deleted_debris: Vec<usize, 10> = Vec::<usize, 10>::new();
+        let mut deleted_debris: Vec<usize, 30> = Vec::<usize, 30>::new();
         for i in 0..self.debris.len() {
             let _ = deleted_debris.push(i);
             self.debris[i].clear_current();
@@ -294,7 +296,12 @@ impl Debris {
     }
 
     fn update_location(&mut self) {
-        self.col = sub1::<BUFFER_WIDTH>(self.col);
+        if self.dx_tick == 0 {
+            self.col = sub1::<BUFFER_WIDTH>(self.col);
+            self.dx_tick = self.dx;
+        } else {
+            self.dx_tick -= 1;
+        }
     }
 
     fn draw_current(&self) {
